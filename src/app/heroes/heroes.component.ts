a8d004435a4d33e6d1f47e5249c999d6b587e7cb
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-03-11 03:52:16 +0200

    Tour of Heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import {
+  /* . . . */
+  NgFor, NgIf, UpperCasePipe,
+  /* . . . */
+} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+  standalone:true,
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+    HeroDetailComponent,
+    RouterModule
+  ],
+})
+export class HeroesComponent implements OnInit {
+
+  constructor( private heroService: HeroService ) { }
+
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+  
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
